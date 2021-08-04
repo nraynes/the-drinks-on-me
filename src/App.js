2c323e67fb8774d7845219532fa4c903ea8a7fe1
mojo9492 <mojo9492@outlook.com>
2021-08-04 12:20:59 -0700

    Add DrinkDetails component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import DrinkDetails from "./components/drink-details/DrinkDetails";
 
 function App() {
   return (
     <div className="App">
-
+      <DrinkDetails/>
     </div>
   );
 }
---
+++ src/components/drink-details/DrinkDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "./DrinkDetails.module.css";
+
+
+function DrinkDetails(props) {
+  return (
+    <div className={styles.drinkDetailsBody}>
+      
+    </div>
+  );
+}
+
+export default DrinkDetails;
