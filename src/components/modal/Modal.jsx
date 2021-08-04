903e314a2ae5724ffb94d186969f19ebdcd5428b
mojo9492 <mojo9492@outlook.com>
2021-08-04 11:31:53 -0700

    model layout

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import styles from "./Modal.module.css"
 
 function Modal(props) {
   return (
-    <div>
-      are you 21
+    
+    <div className={styles.modalBody}>
+      <div className={styles.modalTitle}>
+        Drinks On Me
+      </div>
+      <div className={styles.modalPrompt}>
+        Are you 21+ years old?
+        <div className={styles.modalPromptButtons}>
+          <button>Yes</button>
+          <button>No</button>
+        </div>
+      </div>
     </div>
   );
 }
